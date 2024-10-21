94f236ce20959534f82e8e68c5615fcc066aa257
joewadk <jkabir000@citymail.cuny.edu>
2024-10-20 20:58:47 -0400

    working on ml model. currently training on dataset with epochs=100

--- hand_detect.py
+++ hand_detect.py
@@ -1,11 +1,35 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from dotenv import load_dotenv
+from roboflow import Roboflow
+from ultralytics import YOLO
+import os
+load_dotenv() #environment variables, make a .env file with your ROBOFLOW_API_KEY
+
+
 #roboflow stuff (it just works)
-#rf = Roboflow(api_key=apikey)
-#project = rf.workspace("david-lee-d0rhs").project("american-sign-language-letters")
-#version = project.version(1)
-#dataset = version.download("yolov8")
-#model = YOLO(dataset)
+apikey = os.getenv("ROBOFLOW_API_KEY")
+rf = Roboflow(api_key=apikey)
+
+#uncomment these and move the dataset to /data
+'''
+project = rf.workspace("david-lee-d0rhs").project("american-sign-language-letters")
+version = project.version(6)
+dataset = version.download("yolov8")
+model = YOLO(dataset)
+'''
+# if this isnt ur first time running, then do this:
+
+dataset_path = "data/American-Sign-Language-Letters-6/data.yaml"
+model=YOLO(dataset_path) #fit model with dataset
+results = model.train(data=dataset_path, epochs=3)  # train the model
+
+print(results) #print results
+
+
+
+
+#camera stuff
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Failed to open camera")
