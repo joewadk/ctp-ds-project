a73f9a441b84f2fa6cf813b09e2395e6700377eb
Yared Pena <102680810+YaredPena@users.noreply.github.com>
2024-10-23 04:21:19 -0400

    Streamlit frontend shell

---
+++ Frontend/streamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Title:
+st.title("A Voiceless Voice")
+
+# Header:
+st.header("Things I need to add")
+
+# Things front-end still needs
+st.write("Open CV Screen") 
+st.write("Build Text Display div")
+st.write("Build Voice display div")
+st.write("Build Voice select button")
+st.write("Volume button w/ icon")
